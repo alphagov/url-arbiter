6440af1029e4ed533cb51ad27a813a9e57a9bd09
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-08-08 11:10:06 +0000

    Failing tests for path reservation

---
+++ spec/integration/registering_path_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe "Registering a path", :type => :request do
+
+  describe "for a path that has not previously been registered" do
+
+    it "should reserve a new path and return json details" do
+      put_json "/paths/foo/bar", {"publishing_app" => "publisher"}
+
+      expect(response.status).to eq(201)
+      data = JSON.parse(response.body)
+      expect(data["path"]).to eq("/foo/bar")
+      expect(data["publishing_app"]).to eq("publisher")
+
+      reservation = Reservation.find_by_path("/foo/bar")
+      expect(reservation).to be
+      expect(reservation.publishing_app).to eq("publisher")
+    end
+
+    it "should return 422 and error details for an invalid request" do
+      put_json "/paths/foo/bar", {"publishing_app" => ""}
+
+      expect(response.status).to eq(422)
+      data = JSON.parse(response.body)
+      expect(data["errors"]).to eq({"publishing_app" => ["can't be blank"]})
+
+      expect(Reservation.find_by_path("/foo/bar")).to be_nil
+    end
+  end
+
+  describe "for a path that has previously been registerd" do
+    before :each do
+      @reservation = create(:reservation, :path => "/foo/bar", :publishing_app => "publisher")
+    end
+
+    it "should be successful if the publishing_app matches" do
+      put_json "/paths/foo/bar", {"publishing_app" => "publisher"}
+
+      expect(response.status).to eq(200)
+    end
+
+    it "should 409 with an error if the publishing_app doesn't match" do
+      put_json "/paths/foo/bar", {"publishing_app" => "whitehall"}
+
+      expect(response.status).to eq(409)
+      data = JSON.parse(response.body)
+      expect(data["errors"]).to eq({"base" => ["is already reserved by the 'publisher' app"]})
+
+      @reservation.reload
+      expect(@reservation.publishing_app).to eq("publisher")
+    end
+  end
+
+  it "returns a 400 for a request with invalid json" do
+    put "/paths/foo", "I'm not json", "CONTENT_TYPE" => "application/json"
+
+    expect(response.status).to eq(400)
+  end
+end
