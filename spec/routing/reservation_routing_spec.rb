c6f01f9e3fd6ef39c33264fa723a5c08ef81051b
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-08-13 10:34:52 +0100

    Accept root path `/` for reservation.
    Also changes the routing to be compatible with
    newer versions of Rails.

--- spec/routing/reservation_routing_spec.rb
+++ spec/routing/reservation_routing_spec.rb
@@ -6,13 +6,32 @@ describe "routing of reservation requests", :type => :routing do
       expect(:put => "/paths/foo/bar").to route_to({
         :controller => "reservations",
         :action => "update",
-        :reserved_path => "/foo/bar",
+        :reserved_path => "foo/bar",
       })
     end
 
     it "should not match a reserved_path without a leading /" do
       expect(:put => "/pathsfoo").not_to be_routable
     end
+
+    it "should match the root path" do
+      expect(put: "/paths/").to route_to({
+        controller: "reservations",
+        action: "update",
+      })
+    end
+  end
+
+  context "GET route" do
+    it "should not match a reserved_path without a leading /" do
+      expect(get: "/pathsfoo").not_to be_routable
+    end
+
+    it "should match the root path" do
+      expect(get: "/paths/").to route_to(
+        controller: "reservations",
+        action: "show",
+      )
+    end
   end
 end
-
